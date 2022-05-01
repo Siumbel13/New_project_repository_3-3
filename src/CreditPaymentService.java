6747a5ccae8c42054c60144c26a0b7f1e802d6a9
Siumbel <sumbelragipova@gmail.com>
2022-05-01 14:58:15 +0300

    second commit

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,11 +1,11 @@
 public class CreditPaymentService {
 
-    public void monthlyPayment(int amount, int years) {
-        int month = years * 12; // количесвто
+    public double monthlyPayment(double amount, double years) {
+        double month = years * 12; // количесвто
         double monthlyPercent = 9.99 / (100 * 12); // месячная процентная ставка
         double monthlyPayment = amount * monthlyPercent / (1 - Math.pow((1 + monthlyPercent), ((-1) * month)));
 
-       System.out.println("Ежемесячный платеж: " + monthlyPayment);
+        return monthlyPayment;
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,18 @@ public class Main {
     public static void main(String[] args) {
         CreditPaymentService creditPaymentService = new CreditPaymentService();
 
-        creditPaymentService.monthlyPayment(1_000_000, 3);
+        double monthlyPayment1 = creditPaymentService.monthlyPayment(1_000_000, 1);
+        long payment1 = (long)monthlyPayment1;
+        System.out.println("Ежемесячный платеж : " + payment1 + "руб");
 
+        double monthlyPayment2 = creditPaymentService.monthlyPayment(1_000_000,2);
+        long payment2 = (long)monthlyPayment2;
+        System.out.println("Ежемесячный платеж : " + payment2 + "руб");
+
+        double monthlyPayment3 = creditPaymentService.monthlyPayment(1_000_000,3);
+        long payment3 = (long)monthlyPayment3;
+        System.out.println("Ежемесячный платеж : " + payment3 + "руб");
 
     }
+
 }
